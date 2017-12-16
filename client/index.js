3f256a75c1d4e7b46b71d0755513c9b2f2efb3ca
Claire Johnson <c.eliseee@gmail.com>
2017-12-15 19:04:01 -0800

    Add snake draw functions

--- client/index.js
+++ client/index.js
@@ -6,3 +6,33 @@ const h = 350;
 let score = 0;
 let snake;
 let food;
+
+const drawModule = (function() {
+  const bodySnake = (x, y) => {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+    ctx.strokeStyle = 'darkgreen';
+    ctx.strokeRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+  };
+
+  const pizza = function(x, y) => {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x * snakeSize + 1, y * snakeSize + 1, snakeSize - 2, snakeSize - 2);
+  };
+
+  const scoreText = () => {
+    const score = `Score: ${score}`;
+    ctx.fillStyle = 'blue';
+    ctx.fillText(score, 145, h - 5);
+  };
+
+  const drawSnake = () => {
+    const length = 4;
+    const snake = [];
+    for (let i = 0; i < length; i++) {
+      snake.push({ x: i, y: 0 });
+    }
+  }
+})();
